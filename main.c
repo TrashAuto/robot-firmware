ed29d3b41dee2454e7e3d7fbf7a1f08e1076d6c8
zzaid17 <91937100+zzaid17@users.noreply.github.com>
2025-02-20 23:16:08 -0700

    Moved movement functions and definitions out of main and included in navigation

--- main.c
+++ main.c
@@ -52,14 +52,6 @@
 #include <xc.h>
 #include <p24F16KA101.h>
 
-#define Stop 0
-#define Left 1
-#define Forwards 2
-#define Backwards 3
-#define Right 4
-
-uint8_t movement_state = 0;
-
 int main(void) {
     AD1PCFG = 0xFFFF; /* keep this line as it sets I/O pins that can also be analog to be digital */
     //data as output, output = 0
@@ -121,74 +113,4 @@ void Timer1_init(void){
     T1CONbits.TON = 1;
     TMR1 = 0;
     PR1 = 31250;
-}
-
-//4 is forwards other is back
-//control of angle and distance will be determined through communication with odometer
-//maybe switch modes between manual and automatic
-//look into how to interface with pi
-void move_left(uint8_t angle){
-    LATBbits.LATB4 = 1;
-    LATBbits.LATB7 = 0;
-    LATAbits.LATA4 = 0;
-    LATAbits.LATA2 = 1;
-}
-
-void move_forwards(uint8_t distance){
-    LATBbits.LATB4 = 1;
-    LATBbits.LATB7 = 0;
-    LATAbits.LATA4 = 1;
-    LATAbits.LATA2 = 0;
-}
-
-void move_backwards(uint8_t distance){
-    LATBbits.LATB4 = 0;
-    LATBbits.LATB7 = 1;
-    LATAbits.LATA4 = 0;
-    LATAbits.LATA2 = 1;
-}
-
-void move_right(uint8_t angle){
-    LATBbits.LATB4 = 0;
-    LATBbits.LATB7 = 1;
-    LATAbits.LATA4 = 1;
-    LATAbits.LATA2 = 0;
-}
-
-
-void movement(void){
-    uint8_t angle = 0;
-    uint8_t distance = 0;
-    
-    if (distance > 1){
-        movement_state = Forwards;
-    }else if (distance < -1){
-        movement_state = Backwards;
-    }
-    
-    if (angle > 1){
-        movement_state = Right;
-    }else if (angle < -1){
-        movement_state = Left;
-    }
-    
-    switch(movement_state){
-        case(Stop):
-            break;
-        case(Left):
-            move_left(angle);
-            break;
-            
-        case(Right):
-            move_right(angle);
-            break;
-            
-        case(Forwards):
-            move_forwards(distance);
-            break;
-            
-        case(Backwards):
-            move_backwards(distance);
-            break;
-    }     
 }
